efe4d8edbc2c06f589a19721f0e0b920fc3af95f
nidaftrnie <fitrianienida@gmail.com>
2024-04-20 17:55:37 +0700

    Challenge - 5 - Final

--- route/car.js
+++ route/car.js
@@ -4,12 +4,15 @@ const carController = require("../controller/car");
 const { authMiddleware } = require("../middleware/auth");
 
 /* Add routes */
-router.route("/").get(authMiddleware(["user", "admin"]), carController.getCars).post(authMiddleware(["admin"]), carController.addCar);
+router
+  .route("/")
+  .get(authMiddleware(["user", "admin"]), carController.getCars)
+  .post(authMiddleware(["admin"]), carController.addCar);
 
 router
   .route("/:id")
   .get(authMiddleware(["user", "admin"]), carController.getCar)
-  .putauthMiddleware(["admin"]), (carController.updateCar)
+  .put(authMiddleware(["admin"]), carController.updateCar)
   .delete(authMiddleware(["admin"]), carController.deleteCar);
 
 module.exports = router;
